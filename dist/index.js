4ae55c6b536ef190b9f10de2c12d034d0d9a0f10
Anthony Shaw <anthony.p.shaw@gmail.com>
2023-02-27 14:34:04 +1100

    Add configurable line length limit

--- dist/index.js
+++ dist/index.js
@@ -12972,7 +12972,11 @@ const isBinaryFileSync = (__nccwpck_require__(8451).isBinaryFileSync);
 const fs = __nccwpck_require__(7147);
 const logger = __nccwpck_require__(3038);
 
-function checkFileForTerms(file, expression, terms) {
+const MAX_LINE_LENGTH = 1000;
+
+function checkFileForTerms(file, expression, terms, maxLineLength) {
+    if (isNaN(maxLineLength))
+        maxLineLength = MAX_LINE_LENGTH
     var passed = true;
 
     // build the regular expression
@@ -12982,32 +12986,36 @@ function checkFileForTerms(file, expression, terms) {
     if (!isBinaryFileSync(file)){
         var content = fs.readFileSync(file, 'utf-8').toString().split("\n");
         content.forEach((line, index) => {
-            var matches = line.matchAll(r);
-            if(matches) {
-                passed = false;
+            if (line.length > maxLineLength) {
+                logger.warn(`Line ${index + 1} on ${file} is longer than the limit of ${MAX_LINE_LENGTH} characters, skipping.`);
+            } else {
+                var matches = line.matchAll(r);
+                if(matches) {
+                    passed = false;
 
-                for (const match of matches) {
-                    var termFound = match[0].trim();
-                    
-                    // get alternatives (need to normalize the match to remove the spaces and aditional chars)
-                    var termAlternatives = [];
-                    var regexWhitespace = new RegExp('[\\s_-]', "gi");
-                    var termFoundNormalized = termFound.replace(regexWhitespace, '').toLowerCase();
-                    var termMatch = terms.find(term => term.term.replace(regexWhitespace, '') === termFoundNormalized);
-                    if (termMatch) {
-                        termAlternatives = termMatch.alternatives;
+                    for (const match of matches) {
+                        var termFound = match[0].trim();
+                        
+                        // get alternatives (need to normalize the match to remove the spaces and aditional chars)
+                        var termAlternatives = [];
+                        var regexWhitespace = new RegExp('[\\s_-]', "gi");
+                        var termFoundNormalized = termFound.replace(regexWhitespace, '').toLowerCase();
+                        var termMatch = terms.find(term => term.term.replace(regexWhitespace, '') === termFoundNormalized);
+                        if (termMatch) {
+                            termAlternatives = termMatch.alternatives;
+                        }
+
+                        // add endColumn to logger?  match.index + termFound.length
+                        logger.warn(`${file}\#L${index + 1} ${match.input.trim()}`, file, (index + 1).toString(), match.index.toString(), `Consider replacing term "${termFound}" with an alternative such as "${termAlternatives.join('", "')}"`);
+                        logger.debug(`${file}\#L${index + 1} ${match.input.trim()}`);
+                        // core.warning(`${file}\#L${index + 1}\r\n${match.input.trim()}`,{
+                        //     file: file,
+                        //     startLine: (index+1).toString(),
+                        //     startColumn: match.index.toString(),
+                        //     endColumn: (match.index + termFound.length).toString(),
+                        //     title: `Consider replacing term "${termFound}" with an alternative such as "${termAlternatives.join('", "')}"` 
+                        // });
                     }
-
-                    // add endColumn to logger?  match.index + termFound.length
-                    logger.warn(`${file}\#L${index + 1} ${match.input.trim()}`, file, (index + 1).toString(), match.index.toString(), `Consider replacing term "${termFound}" with an alternative such as "${termAlternatives.join('", "')}"`);
-                    logger.debug(`${file}\#L${index + 1} ${match.input.trim()}`);
-                    // core.warning(`${file}\#L${index + 1}\r\n${match.input.trim()}`,{
-                    //     file: file,
-                    //     startLine: (index+1).toString(),
-                    //     startColumn: match.index.toString(),
-                    //     endColumn: (match.index + termFound.length).toString(),
-                    //     title: `Consider replacing term "${termFound}" with an alternative such as "${termAlternatives.join('", "')}"` 
-                    // });
                 }
             }
         });
@@ -13465,6 +13473,8 @@ async function run() {
       filenames = readFiles.getFilesFromDirectory(dir, excludeFilesList);
     }
 
+    const maxLineLength = parseInt(params.read("maxLineLength"));
+
     filenames.forEach(filename => {
       logger.debug(`Scanning file: ${filename}`);
       //core.startGroup(`Scanning file: ${filename}`);
@@ -13489,7 +13499,7 @@ async function run() {
           core.debug(`Skipping the term '${phrase.term}'`);
       }); */
 
-      passed = checkFileForTerms(filename, nonInclusiveTerms.getTermsRegex(excludeTermsList), list);
+      passed = checkFileForTerms(filename, nonInclusiveTerms.getTermsRegex(excludeTermsList), list, maxLineLength);
 
       //core.endGroup();
     });
